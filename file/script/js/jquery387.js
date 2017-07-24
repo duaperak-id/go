8e66a89cbeff2f0a27ab57e798aa7c41df7f93ab
Angga Saputra <duaperak.id@gmail.com>
2017-07-24 16:19:43 +0700

    Create jquery387.js

---
+++ file/script/js/jquery387.js
@@ -0,0 +1,2 @@
+$(document).ready(function () { if ($("#jangan-di-ganti,.jangan-di-ganti").attr("href") != "https://www.duaperak.id/") {
+        window.location.href = "https://www.duaperak.id/"; }});
